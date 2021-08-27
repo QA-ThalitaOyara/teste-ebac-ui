24358ea0fb824bf4bb624a688e72807b10f4227f
QA-ThalitaOyara <thalita_alv3s@hotmail.com>
2021-08-26 23:48:47 -0300

    Capturando elementos em listas, uso de variaveis(var e let), identificando elementos por ID,CLASS e etc

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -6,7 +6,7 @@ describe('Funcionalidade de Pré Cadastro', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
     afterEach(() => {
-        //cy.screenshot()
+        cy.screenshot()
     });
     it('Deve completar o pré cadastro com sucesso', () => {
         cy.get('.register > :nth-child(1) > label').type(faker.internet.email())
@@ -16,6 +16,23 @@ describe('Funcionalidade de Pré Cadastro', () => {
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
         cy.get('#account_first_name').type(faker.name.firstName())
         cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    }) 
+    it('Deve completar o pré cadastro com sucesso - Utilizando variavéis com -let- para os Fakers', () => {
+        let emailFaker = faker.internet.email()
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker =  faker.name.lastName()
+
+
+        cy.get('.register > :nth-child(1) > label').type(emailFaker)
+        cy.get('#reg_password').type('Submit0205@')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de predutos', () =>{
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    afterEach(() => {
+        //cy.screenshot()
+    }) 
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]') // para identificar um elemento poderia usar # = ID, . = Class
+        // e quanto coloco entre [] posso colocar id, class, css e etc
+        //.first() para capturar o primeiro elemento da lista
+        //.last() para capturar o ultimo elemento da lista
+        //.eq(3) para capturar o 4 item da lista
+        .contains('Ariel Roll Sleeve Sweatshirt').click() // para capturar o elemento pelo nome
+    });
+
+    it('Deve adicionar o produto ao carrinho', () => {
+        cy.get('[class="product-block grid"]').contains('Ariel Roll Sleeve Sweatshirt').click()
+        cy.get('.product_title').should('contain', 'Ariel Roll Sleeve Sweatshirt')
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(5)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 5)
+        cy.get('.woocommerce-message').should('contain','5 × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+
+    });
+
+    it('Deve adicionar o produto ao carrinho - Fazendo Uso de Variaveis', () => {
+        var quantidade = 5
+
+        cy.get('[class="product-block grid"]').contains('Ariel Roll Sleeve Sweatshirt').click()
+        cy.get('.product_title').should('contain', 'Ariel Roll Sleeve Sweatshirt')
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+
+    });
+
+
+});
