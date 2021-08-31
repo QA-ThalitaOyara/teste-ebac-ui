cbf2b3f6f160e1415f977eeaaaf60e2e882801a3
QA-ThalitaOyara <thalita_alv3s@hotmail.com>
2021-08-31 20:40:01 -0300

    PageObjectes e Utilização de Massa de Dados Json

--- cypress/integration/enderecos.spec.js
+++ cypress/integration/enderecos.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+import DadosEndereco from '../fixtures/enderecos.json'
+
 context('Realizar alteração de endereço', () => {
     beforeEach(() => {
         cy.visit('minha-conta/'),
@@ -13,6 +15,39 @@ context('Realizar alteração de endereço', () => {
         EnderecoPage.editarEnderecoFaturamento('Thalita','Alves', 'SPREAD - TECNOLOGIA', 'Brasil', 'Rua Augusta', '10025','São Paulo', 'São Paulo', '85894689', '13-96587568', 'teste@pageobject.com')
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
 
-        // Cadastrar Endereço
     });   
+    it('Devo realizar alteração de endereço - Chamando Arquivo de Dados Json', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            DadosEndereco[0].nome,
+            DadosEndereco[0].sobrenome,
+            DadosEndereco[0].empresa,
+            DadosEndereco[0].pais,
+            DadosEndereco[0].nomeEndereco,
+            DadosEndereco[0].numeroEndereco,
+            DadosEndereco[0].cidade,
+            DadosEndereco[0].estado,
+            DadosEndereco[0].cep,
+            DadosEndereco[0].telefone,
+            DadosEndereco[0].email
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+    });
+    it.only('Deve realizar alteração no endereço de entrega - Usando Page Objects', () => {
+        EnderecoPage.editarEnderecoEntrega('Teste','Sobrenome','TESTE - 123','Bulgária','TESTE ENDERECO','123456','Teste','Burgas','12345789')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
+
+    });
+    it.only('Deve realizar alteração no endereço de entrega - Usando Dados Arquivo Json', () => {
+        EnderecoPage.editarEnderecoEntrega(
+            DadosEndereco[1].nome,
+            DadosEndereco[1].sobrenome,
+            DadosEndereco[1].empresa,
+            DadosEndereco[1].pais,
+            DadosEndereco[1].nomeEndereco,
+            DadosEndereco[1].numeroEndereco,
+            DadosEndereco[1].cidade,
+            DadosEndereco[1].estado,
+            DadosEndereco[1].cep
+        )
+    });
 });
--- cypress/support/page-objects/endereco.page.js
+++ cypress/support/page-objects/endereco.page.js
@@ -18,9 +18,20 @@ class EnderecoPage {
         cy.get('.button').click()
     }
 
-    editarEnderecoEntrega(){
+    editarEnderecoEntrega(nome,sobreNome,empresa,pais,nomeEndereco,numeroEndereco,cidade,estado,cep){
         //elementos + ações
-    
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
+        cy.get(':nth-child(2) > .title > .edit').click()
+        cy.get('#shipping_first_name').clear().type(nome)
+        cy.get('#shipping_last_name').clear().type(sobreNome)
+        cy.get('#shipping_company').clear().type(empresa)
+        cy.get('#select2-shipping_country-container').click().type(pais).get('[aria-selected="true"]').click()
+        cy.get('#shipping_address_1').clear().type(nomeEndereco)
+        cy.get('#shipping_address_2').clear().type(numeroEndereco)
+        cy.get('#shipping_city').clear().type(cidade)
+        cy.get('#select2-shipping_state-container').click().type(estado).get('[aria-selected="true"]').click()
+        cy.get('#shipping_postcode').clear().type(cep)
+        cy.get('.button').click()
 
     }
 
