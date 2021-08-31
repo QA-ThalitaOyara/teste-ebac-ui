cbfc142ac0cd6760a7f04b3ba66b51663404bb19
QA-ThalitaOyara <thalita_alv3s@hotmail.com>
2021-08-31 12:55:31 -0300

    Funcionalidade Endereços, usando PAGE OBJECTS

--- cypress/integration/enderecos.spec.js
+++ cypress/integration/enderecos.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-
+import EnderecoPage from '../support/page-objects/endereco.page'
 context('Realizar alteração de endereço', () => {
     beforeEach(() => {
         cy.visit('minha-conta/'),
@@ -10,6 +10,8 @@ context('Realizar alteração de endereço', () => {
     });
    
     it('Devo realizar alteração de endereço - Chamando o support commands', () => {
+        EnderecoPage.editarEnderecoFaturamento('Thalita','Alves', 'SPREAD - TECNOLOGIA', 'Brasil', 'Rua Augusta', '10025','São Paulo', 'São Paulo', '85894689', '13-96587568', 'teste@pageobject.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
 
         // Cadastrar Endereço
     });   
